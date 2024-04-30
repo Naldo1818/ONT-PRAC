1231c096aa736a2702deedfaab84565baec51176
Ronaldo Cullen Jansen <s223317012@mandela.ac.za>
2024-04-30 17:25:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONT_PRAC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
